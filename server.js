30e0c964d6c01f5672b939b696fac7444811007f
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-11-19 21:14:52 +0700

    Enable cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const Models    = require('./models'); // Add the Models
 
 
 // Server Config
-server.connection(_.pick(Config, ['host', 'port', 'Routes']));
+server.connection(_.pick(Config, ['host', 'port', 'routes']));
 
 // Export the server to be required elsewhere.
 module.exports = server;
